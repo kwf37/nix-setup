d25b3b96f066d7b38cf5739d7e6d75039225b73c
Kenneth Fang <fangkw42@gmail.com>
2023-05-28 22:51:13 -0400

    Initial polybar config and installation

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,18 @@
 import XMonad
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Util.Run
+
+myPolybar :: StatusBarConfig
+myPolybar = statusBarProp "polybar example" (pure xmobarPP)
 
 main :: IO ()
-main = xmonad $ def
+main =
+    xmonad 
+    $ withSB myPolybar
+    $ def
   `additionalKeys`
     [ ((noModMask, xK_Super_L), spawn "rofi -show run")
     ]
