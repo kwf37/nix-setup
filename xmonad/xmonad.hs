8b7910d7cef96b495c80c92ed61a8290722b6910
Kenneth Fang <fangkw42@gmail.com>
2023-05-29 23:38:14 -0400

    Configure layouts that respect polybar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,9 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import XMonad.Layout
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Util.Run
@@ -11,8 +14,12 @@ myPolybar = statusBarProp "polybar example" (pure xmobarPP)
 main :: IO ()
 main =
     xmonad 
+    $ ewmhFullscreen
+    $ ewmh
     $ withSB myPolybar
-    $ def
+    $ docks def {
+        layoutHook = avoidStruts (Tall 1 (3/100) (1/2) ||| Full)
+    }
   `additionalKeys`
     [ ((noModMask, xK_Super_L), spawn "rofi -show run")
     ]
